2646755de19ca17b274b9e303f6fb068f10dd769
akforsyt <akforsyt@uncc.edu>
2017-08-11 10:49:58 -0400

    change email templates to new WooCommerce versions

---
+++ emails/email-order-details.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Order details table shown in emails.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-order-details.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$text_align = is_rtl() ? 'right' : 'left';
+
+do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+
+<?php if ( ! $sent_to_admin ) : ?>
+	<h2><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></h2>
+<?php else : ?>
+	<h2><a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>"><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></a> (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)</h2>
+<?php endif; ?>
+
+<table cellspacing="0" cellpadding="6" style="width: 100%; border: 1px solid #eee;" border="1" bordercolor="#eee">
+<thead>
+<tr>
+<th scope="col" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Item', 'woocommerce' ); ?></th>
+<th scope="col" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
+</tr>
+</thead>
+<tbody>
+<?php echo $order->email_order_items_table( $order->is_download_permitted(), false, ( $order->status=='processing' ) ? true : false ); ?>
+</tbody>
+<tfoot>
+<?php
+if ( $totals = $order->get_order_item_totals() ) {
+$i = 2;
+foreach ( $totals as $total ) {
+if($total['label'] == 'Shipping:') { 
+?><tr>
+<td scope="row" colspan="2" style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['value'] . ' on ' . $_POST['e_deliverydate'];?> </td>
+</tr><?php 
+}
+}
+}
+?>
+</tfoot>
+</table>
+
+<?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
